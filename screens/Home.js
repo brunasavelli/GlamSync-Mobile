5846da417dc0fb077899e9e6cf055c3f4418297d
Alejandra <alejandrabarros.tds1.senai@gmail.com>
2025-05-10 19:16:55 -0300

    feat: finish home page

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Image, ImageBackground, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
@@ -8,44 +8,54 @@ export default function Home() {
     const navigation = useNavigation();
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>WELCOME!</Text>
-            <Image source={require('../assets/logo-semFundo.png')} style={styles.logo} />
-            <Text style={styles.brand}>GlamSync</Text>
-            <Text style={styles.slogan}>Fashion that conects</Text>
-            <Text style={styles.slogan}>Style that impacts</Text>
-            <TouchableOpacity onPress={() => navigation.navigate('Login')} style={styles.button}>
-                <Text style={styles.buttonText}>Log In</Text>
-                <FontAwesome6 name="arrow-right-to-bracket" size={20} color="black" />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('SignUp')} style={styles.button}>
-                <Text style={styles.buttonText}>Sign Up</Text>
-                <FontAwesome name="user-circle-o" size={20} color="black" />
-            </TouchableOpacity>
-        </View>
+        <ImageBackground source={require('../assets/background-mobile-glamsync.png')} style={styles.background}>
+            <View style={styles.container}>
+                <Text style={styles.title}>WELCOME!</Text>
+                <Image source={require('../assets/logoComEscrita.png')} style={styles.logo} />
+                <View style={styles.sloganContainer}>
+                    <Text style={styles.slogan}>Fashion that conects</Text>
+                    <Text style={styles.slogan}>Style that impacts</Text>
+                </View>
+                <TouchableOpacity onPress={() => navigation.navigate('Login')} style={styles.button}>
+                    <Text style={styles.buttonText}>Log In</Text>
+                    <FontAwesome6 name="arrow-right-to-bracket" size={20} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate('SignUp')} style={styles.button}>
+                    <Text style={styles.buttonText}>Sign Up</Text>
+                    <FontAwesome name="user-circle-o" size={20} color="black" />
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    },
     container: {
         flex: 1,
         alignItems: 'center',
-        backgroundColor: '#F79489',
+        justifyContent: 'center',
     },
     title: {
         fontSize: 50,
         color: 'white',
         fontWeight: 'bold',
+        marginTop: 50,
     },
     logo: {
         width: 400,
         height: 400,
         marginTop: 20,
     },
-    brand: {
-        fontSize: 50,
-        color: 'white',
-        fontWeight: 'bold',
+    sloganContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 20,
     },
     slogan: {
         fontSize: 20,
@@ -57,8 +67,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
         backgroundColor: '#fff',
-        width: 250,
-        paddingVertical: 10,
+        width: 215,
+        paddingVertical: 8,
         paddingHorizontal: 30,
         borderRadius: 50,
         marginTop: 15,
