9df660bede3dd3027c4a3358feba10355c6da0f8
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-05-13 15:46:42 -0300

    Feat: Page signUP

--- navigation/HomeStackNavigator.js
+++ navigation/HomeStackNavigator.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "../screens/Home";
 import Login from "../screens/Login";
+import SignUp from "../screens/SignUp";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,8 @@ export default function HomeStackNavigator() {
         <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+
         </Stack.Navigator>
     );
 }
