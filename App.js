45a6f2e5088f6990b690a16faa69590dfda7921c
Alejandra <alejandrabarros.tds1.senai@gmail.com>
2025-05-26 17:03:41 -0300

    refactor: adjust logo

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
 import HomeStackNavigator from "./navigation/HomeStackNavigator";
-import InitialFeed from "./screens/InitialFeed"
+//import InitialFeed from "./screens/InitialFeed"
 
 export default function App() {
     return (
         <NavigationContainer>
-            <InitialFeed />
+            <HomeStackNavigator />
         </NavigationContainer>
     );
 }
