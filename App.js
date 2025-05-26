a5112e7d3a31c9b5c81ad6e9bff1bad38bf6ee1e
Alejandra <alejandrabarros.tds1.senai@gmail.com>
2025-05-26 18:31:23 -0300

    feat: add footer

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
-import HomeStackNavigator from "./navigation/HomeStackNavigator";
+//import HomeStackNavigator from "./navigation/HomeStackNavigator";
 //import InitialFeed from "./screens/InitialFeed"
+import Profile from "./screens/Profile";
 
 export default function App() {
     return (
         <NavigationContainer>
-            <HomeStackNavigator />
+            <Profile />
         </NavigationContainer>
     );
 }
