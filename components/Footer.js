520c98f367eb14d5bcfa922a9ac520884b28768c
Alejandra <alejandrabarros.tds1.senai@gmail.com>
2025-05-26 23:11:10 -0300

    feat: add footer on page

--- components/Footer.js
+++ components/Footer.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
         borderTopRightRadius: 20,
         borderWidth: 1,
         borderColor: '#dcdcdc',
-        position: 'absolute',
+        position: 'fixed',
         bottom: 0,
         left: 0,
     },
